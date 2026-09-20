e4f3a55d1fe9c19076f0a151b9dd7f0fc89de753
spartan-haikieu <hai.kieu@c0x12c.com>
2026-09-20 14:16:06 +0700

    feat: use the banana graduate as the login avatar
    The image is square, so it fills Login's circular crop without distortion.

--- src/configs/user.ts
+++ src/configs/user.ts
@@ -2,7 +2,7 @@ import type { UserData } from "~/types";
 
 const user: UserData = {
   name: "William Kieu",
-  avatar: "img/ui/avatar.jpg",
+  avatar: "img/ui/banana-graduate.png",
   password: ""
 };
 
